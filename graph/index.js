0cb1bb8fe333ae5c75b5a810d83e1d95a1bb82b6
oislasreyes <oislasreyes@gmail.com>
2022-07-12 20:58:09 -0500

    Agregando el metodo showconnections

--- graph/index.js
+++ graph/index.js
@@ -19,6 +19,15 @@ class Graph {
     }
     this.adjacentList[source].push(destination);
   }
+
+  showConnections(source) {
+    const currentNode = this.adjacentList[source];
+    let connections = '';
+    for (let vertex of currentNode) {
+      connections += vertex + ',';
+    }
+    console.log(source, '--->', connections);
+  }
 }
 
 const myGraph = new Graph();
@@ -33,4 +42,5 @@ myGraph.addEdge(1, 3);
 myGraph.addEdge(2, 0);
 myGraph.addEdge(2, 1);
 myGraph.addEdge(3, 2);
-console.log(myGraph.adjacentList);
+myGraph.showConnections(3);
+// console.log(myGraph.adjacentList);
